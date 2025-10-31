31042a08b17702b14d7b7d9447c0801702d8e0de
nobody <github2@invisiblehand.church>
2025-10-31 10:38:38 -0400

    x

---
+++ lib/qx.cc
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
+#include <iostream>
+using namespace std;
+
+string qx(const string& cmd) {
+    array<char, 128> buffer;
+    string result;
+    unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
+    if (!pipe) {
+      cerr << "popen failed" << endl;
+      exit(1);
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
+
+// Usage:
+string output = qx("llvm-config --includedir");
+string files = qx("find /usr/include -name '*.h'");
+
